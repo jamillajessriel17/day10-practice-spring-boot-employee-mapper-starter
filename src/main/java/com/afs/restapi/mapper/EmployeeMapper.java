e3abb431e84be5a3a1cc43d9617be381382a19ab
JESSRIEL JAMILLA <jamillajessriel17@gmail.com>
2023-08-25 11:12:16 +0800

    refactor: create private constructor for employee mapper

--- src/main/java/com/afs/restapi/mapper/EmployeeMapper.java
+++ src/main/java/com/afs/restapi/mapper/EmployeeMapper.java
@@ -5,6 +5,9 @@ import com.afs.restapi.entity.Employee;
 import org.springframework.beans.BeanUtils;
 
 public class EmployeeMapper {
+    private EmployeeMapper(){
+
+    }
 
     public static Employee toEntity(EmployeeRequest employeeRequest){
         Employee employee = new Employee();
